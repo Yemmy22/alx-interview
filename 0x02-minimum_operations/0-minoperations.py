5f7da1b6521afc21d6692d50918ee6d0fc8e0c1b
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-10-24 23:15:33 +0000

    calculates the fewest number of operations needed to result in exactly n H characters in the file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+A minOperations function module.
+"""
+
+
+def minOperations(n):
+    """
+    Returns the fewest number of operations needed
+    to result in exactly n H characters
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        # While n is divisible by the current factor
+        while n % factor == 0:
+            operations += factor  # Add the factor to the count
+            n //= factor  # Divide n by the factor
+        factor += 1  # Move to the next factor
+    
+    return operations

d8fc51bdb6bc755914df228a42e21ce078c249c4
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-11-20 09:04:36 +0000

    Transposes the values of a 3-D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A rotate_2d_matrix function module
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate a 2D matrix 90 degrees clockwise in-place.
+    """
+    n = len(matrix)
+
+    # Step 1: Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Step 2: Reverse each row
+    for row in matrix:
+        row.reverse()
